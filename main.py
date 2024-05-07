961535871f5fadc8cfbc1992d4e86ca3621df13c
Anti4ris98 <142513292+Anti4ris98@users.noreply.github.com>
2024-05-07 21:40:08 +0300

    Multiprocessing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 from pathlib import Path
 from urllib.parse import urlparse, unquote_plus
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from threading import Thread
+from multiprocessing import Process
 
 from jinja2 import Environment, FileSystemLoader
 from pymongo.mongo_client import MongoClient
@@ -81,7 +81,7 @@ class TheBestFramework(BaseHTTPRequestHandler):
 
 
 def run_http_server():
-    httpd = HTTPServer((HTTP_HOST, HTTP_PORT), TheBestFramework)  # noqa
+    httpd = HTTPServer((HTTP_HOST, HTTP_PORT), TheBestFramework)
     try:
         logging.info(f"Server started: http://{HTTP_HOST}:{HTTP_PORT}")
         httpd.serve_forever()
@@ -124,5 +124,9 @@ def run_socket_server():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(threadName)s - %(message)s")
-    Thread(target=run_http_server, name="HTTP_Server").start()
-    Thread(target=run_socket_server, name="SOCKET_Server").start()
+    http_server_process = Process(target=run_http_server, name="HTTP_Server")
+    socket_server_process = Process(target=run_socket_server, name="SOCKET_Server")
+    http_server_process.start()
+    socket_server_process.start()
+    http_server_process.join()
+    socket_server_process.join()
